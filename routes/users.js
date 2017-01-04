d31d0074c1edaba510dc8d11ca92dea9e4708aee
Mahesh J <mahesh.j567@gmail.com>
2017-01-04 16:22:13 +0530

    added hbs, added login route for user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
+'use strict';
+
 var express = require('express');
+var user = require('user');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/login', (req, res, next) => {
+    user.login(req.body.name, req.body.password, function (userObj) {
+        res.json(userObj);
+    }, function (err) {
+        res.status(500).send(err);
+    });
 });
 
 module.exports = router;
